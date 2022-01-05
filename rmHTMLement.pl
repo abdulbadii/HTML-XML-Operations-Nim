dd11e7ef7c73ca5c6d5aa990315ee8010adebd60
abdulbadii <abdulbadii@yandex.com>
2022-01-05 08:30:59 +0700

    optimize

--- rmHTMLement.pl
+++ rmHTMLement.pl
@@ -1,30 +1,31 @@
 #!/usr/bin/perl -w
 use strict;
 
-sub getElem {			# $_[0]= searched elem tag  $_[1] = nth-1 $_[2]= whole elem to search
-	if ($_[2]=~/^(<(?>[a-z]\w*|!DOC)[^>]*+>(?:(?>(?'at'[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>)|(?'node'<(\w++)[^>]*+>(?>(?&at)|(?&node))*+<\/\g-1>))*?(?'tnode'(?=<$_[0]\b[^>]*+>)(?&node))){$_[1]}(?>(?&at)|(?&node))*?)((?&tnode))/) {
+sub getNthElem {			# $_[0]= searched elem  $_[1] = nth-1 $_[2]= whole elem to search
+	if ($_[2]=~/^(<[a-z]\w*[^>]*+>(?:(?'cont'(?>[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>|(?'node'<(\w++)[^>]*+>(?&cont)*+<\/\g-1>)))*?(?'tnode'(?=<$_[0]\b[^>]*+>)(?&node))){$_[1]}(?&cont)*?)((?&tnode))/) {
 		 @{$_[3]}=[$1,$6];
 		 return}
 	return 1
 }
 
 sub getMulNthE {
-	my ($hd,$b)=$_[1]=~ /^(<(?>[a-z]\w*|!DOC)[^>]*+>)(.*)/s;	my $pre='';
+	my $pre='';	my ($h,$b)=$_[1]=~ /^(<[a-z]\w*[^>]*+>)(.*)/s;
 	while ($b=~/\G((?>(?'at'[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>)|(?'node'<(\w++)[^>]*+>(?>(?&at)|(?&node))*+<\/\g-1>))*?)((?=<$_[0]\b[^>]*+>)(?&node))/g) {
-		push (@{$_[3]}, [$_[2].($hd.=$pre.$1), $5]);
+		push (@{$_[3]}, [$_[2].($h.=$pre.$1), $5]);
 		$pre=$5
 	}
 	return !$pre
 }
-# These subs return 1 if cannot find, else 0 and offset & same-name elem pairs in the 4rd arg
+# These subs return 1 if fails to find, else 0 and offset & same-name node pairs in the 4rd arg
 
-my (@res, $MUL);
+my @res;my $MUL;
 sub getE_Path_Rec { my $iOffNode = $_[1];
-	my ($mDepth,$tag,$nth,$path)=$_[0]=~ m#^/(/)?([^[/,]+)(?|\[(\d+|@[^]]+)\]|,(\d+))?(.*)#;
+	my ($tag,$nth,$path)=$_[0]=~ m#^/([^[/,]+)(?|\[(\d+|@[^]]+)\]|,(\d+))?(.*)#;
 	$MUL |= !$nth;
-	for (@$iOffNode) {	my @OffNode;
+	for (@$iOffNode) {
+		my @OffNode;
 		if ($nth) {
-			if (&getElem ($tag, $nth-1, $_->[1], \@OffNode)) {			# offset-node pair return is in @OffNode
+			if (&getNthElem ($tag, $nth-1, $_->[1], \@OffNode)) {			# offset-node pair return is in @OffNode
 				next if @res or $MUL;
 				return 1}
 			${$OffNode[0]}[0]=$_->[0].${$OffNode[0]}[0];
@@ -33,21 +34,20 @@ sub getE_Path_Rec { my $iOffNode = $_[1];
 				next if @res or $MUL;
 				return 1}
 		}
-		if ($path) {	return 1 if &getE_Path_Rec ($path, \@OffNode)			# then propagate it to the next depth
+		if ($path) {	return &getE_Path_Rec ($path, \@OffNode)			# to be propagated to the next depth
 		}	else {				push (@res, @OffNode)
 		}
 	}
 	return
 }
 
-my ($whole, $trPath, @valid, $O, $CP);
+my ($whole, $wBEG, $wTAG, $wEND, $trPath, @valid, $O, $CP);
 if (@ARGV) {
 	$trPath=shift;
 	$O=shift;
 	undef local $/;$whole=<>
 }else {
-	print "Element path is form of Xpath e.g: /html/body/div[1]/div[3]\n\n[1] may be replaced with ,1 e.g: html/body/div,1/div,3\nTo put multiply at once, put one after another delimited by ;\n";
-	print '\nThe element path(s): ';
+	print "Element path is of Xpath form e.g:\n\t\t\t/html/body/div[1]/div[3]\n\n[1] may be replaced with ,1 e.g: html/body/div,1/div,3\nTo put multiply at once, put one after another delimited by ;\nPut element path: ";
 	die "No any Xpath given\n" if ($trPath=<>)=~/^\s*$/;
 	for (split(/;/,$trPath)) {
 		L:if (m#^\s*/?/?([a-z]\w*+(?>\[(?>\d+|@[a-z]+(?:=\w+)?)\]|,\d+)?|@[a-z]\w*)(?://?(?1)?)*+\s*$#i)	{
@@ -59,12 +59,12 @@ if (@ARGV) {
 				}
 				$_=	"$CP/$_";goto L
 			}
-			push (@valid, $_=~ s/\/$//r=~ s/,(\d+)/\[$1\]/gr);
+			push (@valid, s/\/$//r =~s/,(\d+)/\[$1\]/gr);
 		}else {
-			print "'$_' is invalid Xpath\nEdit it or skip it? (E/Enter: Edit  S: Skip.  Else: Abort) ";my $y=<>;
+			print "'$_' is invalid Xpath\nEdit it or skip it? (E/Enter: Edit  S: Skip.  any else: Abort) ";my $y=<>;
 			if($y=~/^e?$/i){
 				print "Edit: ";$_=<>;goto L
-			}elsif ($y=~/^s/i) {next
+			}elsif ($y=~/^s/i) { next
 			}else{	die "Aborted\n"}
 	}}
 	print "HTML/XML file name to process: ";
@@ -72,23 +72,26 @@ if (@ARGV) {
 	$!=1;-e $file or die "\n'$file' not exist\n";
 	$!=2;open R,"$file" or die "\nCannot open '$file'\n";
 	undef local $/;$whole=<R>;close R;
-	print "\nProcessing HMTL document in '$file'...\n";
+	print "\nProcessing HTML document on '$file'...\n";
 }
+
+die "\nCan't parse the HTML with ill form\nBecause likely of unbalanced tag pair\n" unless
+($wBEG,$whole,$wTAG,$wEND)=$whole=~/^(<!DOCTYPE[^>]*+>[^<]*)(<([a-z]\w*)[^>]*+>(?'cont'(?>[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>|<(\w++)[^>]*+>(?&cont)*+<\/\g-1>))*?<\/\g3>)(.*)/s;
 
 my ($E, @path, @fpath, @miss, @short);
 for (sort{length $a cmp length $b} @valid) {
 	if ($E) {
-		print "\nSkip it to process the next path? (Y/Enter: yes. Else: abort) ";
+		print "\nSkip it to process the next path? (Y/Enter: yes. any else: Abort) ";
 		<>=~/^(?:\h*y)?$/i or die "Aborting\n"}
 	@res=();
-	my @i=['',$whole];
-	s#(^/html|(?<=^/html/)body)/#$1\[1\]/#g;
-	if ($E= &getE_Path_Rec ($_, \@i) or not @res and $E=1) {
+	my @in=[$wBEG,$whole];
+	s#^/([a-z]\w*)(/.*)#$2#;
+	if ($wTAG ne $1 or $E=&getE_Path_Rec ($_, \@in) or not @res and $E=1) {
 		push(@miss,$_);
 		print "\nCan't find '$_'"
-	}else{
+	}else {
 		push (@path, [$_, [@res]]);
-		my $cur=$_;			# Optimize removal: filter out longer path whose head is as the shorter one's
+		my $cur=$_;			# Optimize removal: filter out longer path whose head is the same as shorter one
 		for (@short) {
 			goto P if $cur =~ /^\Q$_\E/}
 		push (@fpath, @res);
@@ -99,12 +102,10 @@ for (sort{length $a cmp length $b} @valid) {
 
 if (@miss){
 	if (@path){
-		print "\nKeep processing ones else found? (Y/Enter: yes. any else: abort) ";
+		print "\nKeep processing ones found? (Y/Enter: yes. any else: Abort) ";
 		<>=~s/^\h+//r =~/^y?$/i or die "Aborting\n";
-		print "\nSkipping not found '$_'" for(@miss)
 	}else{	print "\nNothing was done\n";exit
 }}
-print "\nProcessing:";print "\n$_->[0]" for(@path);
 unless	(@ARGV){
 	print "\nWhich operation will be done :\n- Remove\n- Get\n(R: remove. Else: just get it) ";
 	$O=<>=~s/^\h+//r=~ s/\s+$//r;
@@ -112,6 +113,7 @@ unless	(@ARGV){
 	my $of=<>=~s/^\h+//r=~ s/\s+$//r;
 	open W,">","$of" or die "Cannot open '$of'\n" if($of)
 }
+print "\nProcessing the path:";print "\n$_->[0]" for(@path);
 
 for ($O){
 if (! /^r/i) {
@@ -122,10 +124,11 @@ if (! /^r/i) {
 	fileno W? print W $o:print $o;
 	last
 }
-# Removal, etc is from long to shorter el. offset of array fpath, so they're sorted descendingly
+# Removal, etc is from long to shorter el. offset of array fpath, so sort them descendingly
 @fpath=sort {length $b->[0] <=> length $a->[0]} @fpath;
 
+$whole=$wBEG.$whole;
 $whole=~ s/\A(\Q$_->[0]\E)\Q$_->[1]\E(.*)\Z/$1$2/s	for (@fpath);
-fileno W? print W $whole:print "\nRemoval result:\n$whole"
+fileno W? print W $whole:print "\n\nRemoval result:\n$whole"
 }
 close W;
