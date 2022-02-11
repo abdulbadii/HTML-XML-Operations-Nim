84a0aff969be37d6a75cdb3e19843b9c8d0e0e00
abdulbadii <abdulbadii@yandex.com>
2022-02-11 20:02:06 +0700

    fix

--- rmHTMLement.pl
+++ rmHTMLement.pl
@@ -2,18 +2,17 @@
 use strict;
 
 # HTML regexes
-my $HEAD= qr/<[a-z]\w*+[^>]*+>/;
-my $AT=qr{(?>[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>)};			# Asymetric tag/text content
-my $A= qr/$AT*+/;
-my $CT= qr{((?>$AT|<(\w++)[^>]*+>(?-2)*+</\g-1>))};			# Content of node in lazy mode repetition or none
-my $C= qr/$CT*?/;
-my $ND= qr{<(?'t'[a-z]\w*+)[^>]*+>$CT*+</\g{t}>|<[a-z]\w*+.*?/>};	# Node that may have the content or head closing 
+my $HEAD= qr/^<(?>[a-z]\w*+|!DOCTYPE)[^>]*+>/;
+my $A=qr{(?>[^<>]|<(?>meta|link|input|img|hr|base)\b[^>]*+>)};			# Asymetric tag/text content
+my $C= qr{((?>$A|<(\w++)[^>]*+>(?-2)*+</\g-1>))};			# Content of node in lazy mode repetition or none
+my $ND= qr{(<([a-z]\w*+)[^>]*+>(?>$A|[^/>]*+/>|(?-2))*+</\g-1>)};
+#|<[a-z]\w*+.*?/>};	# Node that may have the content or head closing 
 
 sub getNthEAtt {		# $_[0] searched el  $_[1]=nth/nth backw  $_[2] el under which to search  $_[4] nth backw $_[5] attr
 	# obtain max nth +1 to solve backward nth
 	if ($_[4]) {
-		my $c=1; $_[2]=~/^$HEAD(?:$C(?=<$_[0]\b)$ND(?{ ++$c }))+/;	$_[1]=$c-$_[4]}
-	return not $_[2]=~ /^($HEAD(?:$C(?=<$_[0]\b$_[5])($ND)){$_[1]})(?{ @{$_[3]}=[substr($1,0,-length($4)), $4] })/
+		my $i=1; $_[2]=~/$HEAD(?:$C(?=<$_[0]\b)$ND(?{ ++$i }))+/;	$_[1]=$i-$_[4]}
+	return not $_[2]=~ /($HEAD(?:$C*?(?=<$_[0]\b$_[5])($ND)){$_[1]})(?{ @{$_[3]}=[substr($1,0,-length($4)), $4] })/
 }
 
 sub getENthAllAtt {		# $_[1] el under which to search  $_[2] its offset  $_[4]) nth range pos $_[5] attr $_[6] all nodes 
@@ -21,15 +20,15 @@ sub getENthAllAtt {		# $_[1] el under which to search  $_[2] its offset  $_[4]) 
 	if ($_[4]) {
 		my ($lt,$e,$n)= $_[4]=~ /(?>(<)|>)(=)?(\d+)/;
 		$b = $lt?	$e? "{$n}" : '{'.--$n.'}'	: ($a=$e? $n : $n+1, $b) }
-	return not $_[1]=~ /^($HEAD)
+	return not $_[1]=~ /($HEAD)
 	(?{ $OFF=$_[2].$1 })
-	(?: ($C) (?(?{$all}) | (?=<$_[0]\b$att)) ($ND)
+	(?: ($C*?) (?(?{$all}) | (?=<$_[0]\b$att)) ($ND)
 	(?{ if (++$i>=$a) {	push (@{$_[3]}, [$OFF.($off.=$2), $5]); $off.=$5 } }) )$b /x
 }
 
 # Node regex to count depth
 my ($MAX,$DTH);
-my $N= qr/(?{ $MAX=0 })(<(\w++)[^>]*+>(?{ $MAX=$DTH if ++$DTH>$MAX })(?>$AT|(?-2))*+<\/\g-1>(?{--$DTH}))/;
+my $N= qr/(?{ $MAX=0 })(<(\w++)[^>]*+>(?{ $MAX=$DTH if ++$DTH>$MAX })(?>$A|(?-2))*+<\/\g-1>(?{--$DTH}))/;
 
 sub getAllDepth {		# $_[1] nth/nth bckwrd  $_[2] search space element
 															# $_[4] its offset  $_[5] depth  $_[6] nth bckwrd
@@ -38,10 +37,10 @@ sub getAllDepth {		# $_[1] nth/nth bckwrd  $_[2] search space element
 	while (@curNode) {
 		for $onref (@curNode) {
 			if ($_[6]) {
-				my $c=1; $onref->[1]=~ /^$HEAD(?:$C(?=<$_[0]\b)$ND(?{ ++$c }))+/;	$_[1]=$c-$_[6]}
+				my $i=1; $onref->[1]=~ /$HEAD(?:$C*?(?=<$_[0]\b)$ND(?{ ++$i }))+/;	$_[1]=$i-$_[6]}
 			$onref->[1]=~
-			/^($HEAD) (?{ $offset=$1 }) (?:
-			(?: ($A) (?{$offs=$offset.=$2 })
+			/($HEAD) (?{ $offset=$1 }) (?:
+			(?: ($A*+) (?{$offs=$offset.=$2 })
 			(?: (?!<$_[0]\b) (?'n'$N)
 			(?{ push (@nd, [$onref->[0].$offset, $+{n}]) if $MAX>$min;
 			$offset.=$+{n} })
@@ -51,7 +50,7 @@ sub getAllDepth {		# $_[1] nth/nth bckwrd  $_[2] search space element
 			$offset=$offs.$+{nd} })
 			) {$_[1]}
 			(?{ push (@$ret, [$onref->[0].$offs, $+{nd}]) if $MAX>=$min })
-			(?: (?'a'$A) (?{ $offset.=$+{a} })
+			(?: (?'a'$A*+) (?{ $offset.=$+{a} })
 			(?'z'$N) (?{ push (@nd, [$onref->[0].$offset, $+{z}]) if $MAX>$min; $offset.=$+{z} })
 			)*/x
 		}
@@ -69,8 +68,8 @@ sub getAllDepNthRnAtt	{				# in every nth or positon within range
 				my ($lt,$e,$n)= $_[5]=~ /(?>(<)|>)(=)?(\d+)/;
 				$b = $lt?	$e? "{$n}" : '{'.--$n.'}': ($a=$e? $n : $n+1, $b)	}
 			$onref->[1]=~
-			/^($HEAD)(?{$offset=$1}) (?:
-			($A)
+			/($HEAD)(?{$offset=$1}) (?:
+			($A*+)
 			(?{$offset.=$2})
 			(?:(?=<$_[0]\b$att (?{$M=1}))?
 			(?'n'$N) (?{ if ($MAX>=$min) {
@@ -91,8 +90,8 @@ sub getAllDepthAatt {	# $_[0] attribute  $_[1] el under which to search  $_[2] i
 	while (@curNode) {
 		for $onref (@curNode) {
 			$onref->[1]=~
-			/^($HEAD)(?{$offset=$1}) (?:
-			($A)
+			/($HEAD)(?{$offset=$1}) (?:
+			($A*+)
 			(?{$offset.=$2})
 			(?: (?=<\w+$att (?{$M=1}) )?
 			(?'n'$N)
@@ -112,14 +111,15 @@ sub getAllDepthAatt {	# $_[0] attribute  $_[1] el under which to search  $_[2] i
 my @res;
 sub getE_Path_Rec {			# path,  offset - node pair
 	my ($AllDepth, $tag, $nth,$nrev,$range, $attg, $aatt, $allnode, $path) = $_[0] =~
-	m{ ^(/)?/ (?> ([^/@*[]+) (?> \[ (?>([1-9]+ | last\(\)-([1-9]+)) | position\(\)(?!<1)([<>]=?\d+) | @(\*| [^]]+) ) \] )? | @([a-z]\w*[^/]* |\*) | (\*) ) (.*) }x;
-	$attg=$attg? '\s+'.($attg eq '*'? '\S' : $attg) :'';	$aatt=$aatt? '\s+'.($aatt eq '*'? '\S' : $aatt) :'';
+	m{ ^/ (/)? (?> ([^/@*[]+) (?> \[ (?>([1-9]+ | last\(\)-([1-9]+)) | position\(\)(?!<1)([<>]=?\d+) | @(\*| [^]]+) ) \] )? | @([a-z]\w*[^/]* |\*) | (\*) ) (.*) }x;
+	$attg=$attg? '\s+'.($attg eq '*'? '\S' : $attg) :'';
+	$aatt=$aatt? '\s+'.($aatt eq '*'? '\S' : $aatt) :'';
 	for (@{$_[1]}) {
 		my @OffNode;
 		if ($AllDepth) {
-			my $depth=split /\//,$path;
-			if ($tag?	$nth?									# offset-node pair return is in @OffNode..
-					getAllDepth ($tag, $nth, $_->[1], \@OffNode, $_->[0], $depth, $nrev)
+			my $depth=split /\//, $path=~ s/^\/\h*//r;
+			if ($tag?	$nth?
+					getAllDepth ($tag, $nth, $_->[1], \@OffNode, $_->[0], $depth, $nrev)					# offset-node pair return is in @OffNode..
 					: getAllDepNthRnAtt ($tag, $_->[1], $_->[0], \@OffNode, $depth, $range, $attg)
 				: getAllDepthAatt ($aatt, $_->[1], $_->[0], \@OffNode, $depth) ){
 				next if \$_ != \${$_[1]}[-1];											# no error return yet check if there's next must-search
@@ -177,17 +177,16 @@ if (@ARGV) {
 	undef local $/;$whole=<R>;close R;
 	$|=1; print "\nChecking HTML document '$file'... "
 }
-die "can't parse it due to its ill-formed of HTML unbalanced tag pair\n" unless $whole=~m{^\s*((?:<\?xml\b[^>]*+>\s*)?<!DOCTYPE[^>]*+>[^<]*)($ND)$C};
-my @in=[$1,$2]; my $firsTAG=$3;
-
+die "can't parse it due to its ill-formed of HTML unbalanced tag pair\n" unless $whole=~ /^
+(\s* (?:<\?xml\b[^>]*+>\s*)?) (<(!DOCTYPE)[^>]*+>[^<]* $ND $C*$ ) /x;
+my @in=[  $1, $2."</$3>" ];
 my ($ER, @path, @fpath, @miss, @short);
 for (sort{length $a cmp length $b} @valid) {
 	if ($ER) {
 		print "\nSkip it to process the next path? (Y/Enter: yes. any else: Abort) ";
 		<>=~/^(?:\h*y)?$/i or die "Aborting\n"}
 	@res=();
-	m{^/([a-z]\w*)(/.*)};
-	if ( $1 ne $firsTAG or $ER=getE_Path_Rec ($2, \@in)) {
+	if ( $ER = getE_Path_Rec ($_, \@in)) {
 		push(@miss,$_);
 		print "\nCan't find '$_'"
 	}else {
@@ -231,3 +230,7 @@ $whole=~ s/\A(\Q$_->[0]\E)\Q$_->[1]\E(.*)\Z/$1$2/s	for (@fpath);
 fileno W? print W $whole:print "\n\nRemoval result:\n$whole"
 }
 close W;
+#$r=qr/ fo(?:o|k|l)\h*bar/ ; $s = 'fo(?:o|k|l)\h*bar'  ;cmpthese( 0, {
+  #re , sub { 'foobar'=~$r },
+  #str , sub { 'foobar'=~/$r/ },
+#})
